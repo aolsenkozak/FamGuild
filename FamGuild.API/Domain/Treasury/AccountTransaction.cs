f7fc62778e25468bbf903a3be21160207f865021
aolsenkozak <aolsen-kozak@proton.me>
2025-11-25 20:18:47 -0500

    Forgot to add actual AccountTransaction class

---
+++ FamGuild.API/Domain/Treasury/AccountTransaction.cs
@@ -0,0 +1,72 @@
+﻿using FamGuild.API.Domain.Common.ResultPattern;
+using FamGuild.API.Domain.Treasury.Common;
+
+namespace FamGuild.API.Domain.Treasury;
+
+public class AccountTransaction
+{
+    public Guid Id { get; private set; }
+    public EntryClassification Classification { get; private set; }
+    public string Name { get; private set; } = string.Empty;
+    public Money Amount { get; private set; }
+    public string Category { get; private set; } =  string.Empty;
+    public DateTime DateOccurred { get; private set; }
+    public AccountTransactionStatus Status { get; private set; } = AccountTransactionStatus.Pending;
+    public Guid? RecurringTransactionId { get; private set; }
+
+    private AccountTransaction(EntryClassification classification, string name, Money amount, 
+            string category, DateTime dateOccurred, AccountTransactionStatus status, 
+            Guid? recurringTransactionId = null)
+    {
+        Id = Guid.NewGuid();
+        Classification = classification;
+        Name = name;
+        Amount = amount;
+        Category = category;
+        DateOccurred = dateOccurred;
+        Status = status;
+        RecurringTransactionId = recurringTransactionId;
+    }
+
+    public static Result<AccountTransaction> CreateFromRecurringTransaction(
+        RecurringTransaction recurringTransaction, 
+        DateTime dateOccurred, AccountTransactionStatus status)
+    { 
+        return Result.Success(new AccountTransaction(
+            recurringTransaction.Classification, 
+            recurringTransaction.Name, 
+            recurringTransaction.Amount, 
+            recurringTransaction.Category,
+            dateOccurred,
+            status,
+            recurringTransaction.Id
+            ));
+    }
+
+    public static Result<AccountTransaction> Create(EntryClassification classification,
+        string name, Money amount,
+        string category, DateTime dateOccurred, AccountTransactionStatus status)
+    {
+        if (amount.Value < 0)
+        {
+            var error = new Error("BadRequest", "Amount must be greater than or equal to zero.");
+            return Result.Failure<AccountTransaction>(error);
+        }
+        
+        return Result.Success(new AccountTransaction(
+            classification, 
+            name, 
+            amount, 
+            category,
+            dateOccurred,
+            status
+        ));
+        
+    }
+    
+    
+
+    private AccountTransaction()
+    {
+    }
+}
---
+++ FamGuild.API/Domain/Treasury/Common/AccountTransactionStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace FamGuild.API.Domain.Treasury.Common;
+
+public enum AccountTransactionStatus
+{
+    Pending,
+    Confirmed,
+}
