d5482a4750fc82103210f2d9d4abdcba0546a364
aolsen-kozak <aolsen-kozak@proton.me>
2026-01-03 22:34:49 -0500

    Starting up a Blazor UI project

--- FamGuild.Test/obj/Debug/net10.0/FamGuild.Test.AssemblyInfo.cs
+++ FamGuild.Test/obj/Debug/net10.0/FamGuild.Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FamGuild.Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a45fb2c16c9bc0bb565f936839c17af2ddc26c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b92703caf36d8e68c22f6f7170c566c069bbf53")]
 [assembly: System.Reflection.AssemblyProductAttribute("FamGuild.Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FamGuild.Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
